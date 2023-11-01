77f9c0b5efb23bcd48419b303d7d780b97202541
JennyFrost <78221793+JennyFrost@users.noreply.github.com>
2023-11-01 21:15:24 +0200

    Update connect_to_api.py

--- connect_to_api.py
+++ connect_to_api.py
@@ -1,7 +1,13 @@
 from typing import List
 import requests
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)
 
 
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
 def get_random_questions(url: str, n: int) -> List[tuple]:
     url += f"?count={n}"
     question_dicts = requests.get(url).json()
